7437a9efd5e85b095c6f4743352800de6729f072
PacoLeUnique <mougeot.axel@orange.fr>
2025-06-03 18:17:03 +0200

    Ajout options vitesse/inskipable/autoskip

--- script/Textbox/textbox.gd
+++ script/Textbox/textbox.gd
@@ -6,8 +6,11 @@ extends CanvasLayer
 @onready var textbox_image : TextureRect = $TexboxContainer/MarginContainer/HBoxContainer/MarginContainer/CharacterImage
 var tween : Tween ## Tween pour l'animation du texte.
 
-## La durée que met chaque caractère à s'afficher dans la boite de dialogue.
-var CHAR_DISPLAY_DURATION = 0.05
+## La (durée en millisecondes) que met chaque caractère à s'afficher dans la boite de dialogue.
+const CHAR_DISPLAY_DURATION = 50
+
+## La vitesse de défilement du texte. 100 = "100% de la vitesse normale".
+var vitesse_defilement : float = 100
 
 ## Une Textbox peut se trouver dans 3 états différents : [br]
 ## - State.[b]READY[/b] : La Textbox est en attente, pret à afficher sa prochaine ligne de dialogue. [br]   
@@ -22,6 +25,13 @@ enum State {
 	FINISHED,
 }
 
+# Masques pour récupérer les flags de TextboxContent.skip_options.
+const INSKIPABLE_FLAG = 0b1
+const AUTOSKIP_FLAG = 0b01
+
+
+var autoskip : bool = false		## Détermine si la boite de dialogue se ferme automatiquement après la fin du texte.
+var inskipable : bool = false	## Détermine si le texte défilant peut être passé directement ou non.
 
 ## L'état courant de la Textbox. [br]
 ## Voir [enum State]
@@ -45,14 +55,14 @@ func _process(_delta: float) -> void:
 				display_text()
 			
 		State.WRITING:
-			if Input.is_action_just_pressed("accept"):
+			if Input.is_action_just_pressed("accept") and not inskipable:
 				tween.stop()
 				label.visible_ratio = 1
 				end_symbol.text = "v"
 				current_state = State.FINISHED
 				
 		State.FINISHED:
-			if Input.is_action_just_pressed("accept"):
+			if Input.is_action_just_pressed("accept") or autoskip:
 				if queue.is_empty():
 					hide_textbox()
 				else:
@@ -67,6 +77,7 @@ func hide_textbox():
 func flush_textbox():
 	end_symbol.text = ""
 	label.text = ""
+	textbox_image.hide()
 
 func show_textbox():
 	textbox_container.show()
@@ -75,18 +86,44 @@ func add_to_queue(content : TextboxContent):
 	queue.push_back(content)
 
 func display_text():
-	current_state = State.WRITING
 	
 	var content : TextboxContent = queue.pop_front()
-	textbox_image.texture = content.texture
-	label.text = content.texte
+	_load_content(content)
 	
 	label.visible_ratio = 0
 	
 	tween = create_tween()
 	tween.finished.connect(_on_tween_finished)
-	tween.tween_property(label, "visible_ratio", 1, CHAR_DISPLAY_DURATION * len(label.text))
+	
+	var duration = CHAR_DISPLAY_DURATION * len(label.text) / 1000.0
+	duration = duration * (100/vitesse_defilement)
+	
+	print("texte :", label.text, ", len : ", len(label.text))
+	print("vitesse défilement :", vitesse_defilement)
+	print("duration : ", duration)
+	
+	current_state = State.WRITING
+	tween.tween_property(label, "visible_ratio", 1, duration)
 	tween.play()
+
+
+## Charge les données de la textbox dans les variables internes.
+func _load_content(content : TextboxContent):
+	
+	# Affichage image
+	if content.texture != null:
+		textbox_image.texture = content.texture
+		textbox_image.show()
+	
+	#Texte
+	label.text = content.texte
+	
+	#Vitesse défilement du texte
+	vitesse_defilement = content.vitesse_défilement_texte
+
+	# Skip options
+	autoskip = content.skip_options & AUTOSKIP_FLAG
+	inskipable = content.skip_options & INSKIPABLE_FLAG
 
 
 func _on_tween_finished():
--- script/Textbox/textbox_content.gd
+++ script/Textbox/textbox_content.gd
@@ -2,4 +2,16 @@ extends Resource
 class_name TextboxContent
 
 @export var texture : Texture
-@export var texte : String
+@export_multiline var texte : String
+
+# TextboxManager : general_font
+
+#Font
+#Custom font
+#Font color
+#Font size
+
+
+@export_group("Avancé") 
+@export_range(1,500, 1, "or_greater") var vitesse_défilement_texte : float = 100
+@export_flags("Inskipable:1", "Autoskip:2") var skip_options : int = 0 
