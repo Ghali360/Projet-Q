3b48b8f1bb2a00178da8361c4446b335b90d4076
Paco <mougeot.axel@orange.fr>
2025-06-30 20:07:08 +0200

    renommage interaction_component en interaction_manager

--- script/trail.gd
+++ script/trail.gd
@@ -1,8 +1,12 @@
 extends Line2D
 class_name Trails
+
+const FREQUENCE_TIMER : float = 0.05
+
 var Trace : bool = false
 var NbrP : int = 0
-var timer : float = 0.1
+var timer : float = FREQUENCE_TIMER
+
 
 func _process(delta: float) -> void:
 	if Trace:
@@ -12,7 +16,8 @@ func _process(delta: float) -> void:
 			add_point(pos)
 			NbrP += 1 
 			print("point"+str(NbrP))
-			timer = 0.1
+			timer = FREQUENCE_TIMER
+
 
 func _get_position():
 	return get_global_mouse_position()
